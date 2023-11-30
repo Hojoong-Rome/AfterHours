375f3e346466b0d02a20a4aeb5a698b6d9d991a1
Kim tae sung <xofhtm9804@gmail.com>
2023-11-30 19:54:13 +0900

    프론트단 프레임워크 및 라이브러리 설정

---
+++ client/AfterHours/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
+import './App.css'
+
+const App = () => {
+  return (
+    <RecoilRoot>
+      
+    </RecoilRoot>
+  )
+}
+
+export default App
